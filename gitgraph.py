b676493f38f28e37613d013e92465da2bfe62399
Romain Porte <microjoe@mailoo.org>
2014-10-05 15:20:30 +0200

    Implemented Github-like activity graph

--- gitgraph.py
+++ gitgraph.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import datetime
 import re
 
@@ -91,5 +92,94 @@ def stuff():
     print('</svg>')
 
 
+def activity_graph(repos):
+
+    dirpath = os.getcwd()
+    repo_paths = [os.path.join(dirpath, r) for r in repos]
+
+    nb_commits = 0
+
+    dct = {}
+    for path in repo_paths:
+        try:
+            gitpath = pygit2.discover_repository(path)
+        except KeyError:
+            continue
+
+        repo = pygit2.Repository(gitpath)
+        last = repo[repo.head.target]
+
+        for commit in repo.walk(last.id, pygit2.GIT_SORT_TIME):
+            date = datetime.datetime.fromtimestamp(commit.commit_time)
+            delta = datetime.datetime.today() - date
+            daydelta = delta.days
+
+            if daydelta not in dct:
+                dct[daydelta] = 0
+            dct[delta.days] += 1
+
+            nb_commits += 1
+
+    # Computing list of days
+    days = []
+    for i in range(max(dct.keys()) + 1):
+        if i in dct:
+            days.append(dct[i])
+        else:
+            days.append(0)
+
+    draw_activity(days)
+
+
+def compute_color(nb, m):
+    ratio = nb / m
+
+    if ratio > 0.75:
+        color = '1e6823'
+    elif ratio > 0.5:
+        color = '44a340'
+    elif ratio > 0.25:
+        color = '8cc665'
+    elif ratio > 0:
+        color = 'd6e685'
+    else:
+        color = 'eee'
+
+    return '#{}'.format(color)
+
+
+def draw_activity(days):
+    box_height = 10
+    box_width = 10
+    max_commits = max(days)
+    spacing = 3
+    vertical_boxes = 7
+
+    height = int(box_height * (vertical_boxes + spacing))
+    width = int((len(days) / vertical_boxes + 1) * (box_width + spacing))
+
+    print(
+        '<?xml version="1.0" encoding="utf-8" ?>'
+        '<svg xmlns="http://www.w3.org/2000/svg" width="{}" height="{}">'
+        '<title>test</title>'.format(width, height)
+    )
+
+    for n, day in enumerate(reversed(days)):
+        x = int(n / vertical_boxes) * (10 + spacing)
+        y = (n % vertical_boxes) * (10 + spacing)
+
+        print(
+            '<g><rect x="{}" y="{}" width="10" height="10" style="fill:{}" /><title>{} {}</title></g>'
+            .format(
+                x, y,
+                compute_color(day, max_commits),
+                datetime.datetime.now() - datetime.timedelta(days=len(days)-(n+1)),
+                '{} commits'.format(day)
+            )
+        )
+
+    print('</svg>')
+
+
 if __name__ == "__main__":
-    stuff()
+    activity_graph(sys.argv[1:])
